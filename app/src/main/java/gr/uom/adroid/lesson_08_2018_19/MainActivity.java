2fa719463375e215e10653cfa46130df371247f7
teohaik <teohaik@gmail.com>
2018-11-18 01:03:12 +0200

    finished requesting user permission

--- app/src/main/java/gr/uom/adroid/lesson_08_2018_19/MainActivity.java
+++ app/src/main/java/gr/uom/adroid/lesson_08_2018_19/MainActivity.java
@@ -1,17 +1,30 @@
 package gr.uom.adroid.lesson_08_2018_19;
 
+import android.Manifest;
 import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private static final int REQUEST_CODE_READ_CONTACTS = 1;
+    private static boolean READ_CONTACTS_GRANTED = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +33,17 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        int hasReadContactPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS);
+        Log.d(TAG, "HAS Read Contact Permissions "+hasReadContactPermission);
 
+        if(hasReadContactPermission == PackageManager.PERMISSION_GRANTED){
+            Log.d(TAG, "Permission Granted");
+            READ_CONTACTS_GRANTED = true;
+        }
+        else{
+            Log.d(TAG, "Requesting permission");
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CONTACTS}, REQUEST_CODE_READ_CONTACTS);
+        }
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -29,9 +52,49 @@ public class MainActivity extends AppCompatActivity {
                 String[] projection = {ContactsContract.Contacts.DISPLAY_NAME_PRIMARY};
 
                 ContentResolver contentResolver = getContentResolver();
+                Cursor cursor = contentResolver.query(ContactsContract.Contacts.CONTENT_URI,
+                        projection,
+                        null,
+                        null,
+                        ContactsContract.Contacts.DISPLAY_NAME_PRIMARY);
+
+                if(cursor != null) {
+                    List<String> contacts = new ArrayList();
+                    while (cursor.moveToNext()) {
+                        String contact = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME_PRIMARY));
+                        contacts.add(contact);
+                        Log.d(TAG, "contact - "+contact);
+                    }
+                }
 
             }
         });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE_READ_CONTACTS: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                    Log.d(TAG, "Permission Granted!");
+                    READ_CONTACTS_GRANTED = true;
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                    Log.d(TAG, "Permission refused");
+
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
     }
 
     @Override
